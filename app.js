fb702fe024d659fcfa276b57f65f0a033d1a2137
DianaMagallanes <dianakarina2003@example.com>
2022-04-21 22:36:55 -0700

    Agregando nueva ruta

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ app.get('/', (req,res) =>{
     res.send('Hola mundo :)')
 })
 
+//nueva ruta
+app.get('/launchx', (req,res) =>{
+    res.send('Mensaje para Launch X')
+})
+
 //con esto se inicializa app
 app.listen(port,() =>{
     console.log(`Example app listening on port ${port}`)
